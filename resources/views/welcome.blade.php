a83247b71eea47c4da93b71fd4e0da1a1920421d
Dwiky Andika <dikadikcut@gmail.com>
2023-11-26 22:50:05 +0700

    footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,39 +85,52 @@
   </div>
 </div>
 <footer>
-        <div class="py-10 bg-yellow-600 text-red-900">
+        <div class="p-10 bg-yellow-600 text-red-900">
+            <div class="w-40 ml-5">
+            {{-- ga ada gambar --}}
+                <img src="" alt="">
+            </div>
             <div class="max-w-7xl mx-auto">
-                <div class="flex justify-center gap-5">
-                    <a class="ml-10 mt-10 text-lg" href="">Home</a>
-                    <a class="ml-10 mt-10 text-lg" href="">About</a>
-                    <a class="ml-10 mt-10 text-lg" href="">Menu</a>
-                    <a class="ml-10 mt-10 text-lg" href="">Contact Us</a>
+                <div class="grid grid-cols-2">
+                    <div class="grid grid-cols-4 px-5">
+                        <a class="ml-10 mt-10 text-sm font-bold" href="">Home</a>
+                        <a class="ml-10 mt-10 text-sm font-bold" href="">About</a>
+                        <a class="ml-10 mt-10 text-sm font-bold" href="">Menu</a>
+                        <a class="ml-10 mt-10 text-sm font-bold" href="">Contact Us</a>
+                        <div class="grid grid-cols-2 items-center gap-64 pl-20 py-5">
+                            <div class="w-max">
+                                <h1 class="text-xl font-bold">Contact Us</h1>
+                                <p class="py-2 font-semibold">Most calendars are design<br>for teams. Slate is designed<br>for freelancers</p>
+                            </div>
+                            <div class="w-max py-5">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                                </svg>
+                                <p class="font-semibold py-2">Jalan Perkasa sejahtera<br>no 123, Serpong<br>Kab.Tangerang</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                </svg>
+                                <p class="font-semibold py-2">BBCTour@gmail.com</p>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+                                </svg>
+                                <p class="font-semibold py-2">(021) 441-571</p>
+                            </div>
+                            
+                        </div>
+                        {{-- ga ada gambar --}}
+                    </div> 
+                    <a href="#" class="max-w-md mx-auto my-auto"><img src="" alt=""></a>
                 </div>
-                <div class="mt-16 h-px max-h-full opacity-50 border border-black">
+                
+                <div class="h-px max-h-full opacity-50 border border-black">
                     <div class="flex justify-left items-center gap-5">
-                        <p class="text-blue-950 mt-1.5 font-sans font-bold">© 2021 | All Rights Reserved.</p>
-                        <div class="flex justify-center items-center gap-5 mt-2 mx-80">
-                            
-                            <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
-                            </svg></a>
-                            
-                            <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
-                            </svg></a>
-
-                            <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
-                            </svg></a>
-
-                            <a href=""><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
-                            </svg></a>
-
-                        </div>                                  
+                        <p class="text-blue-950 mt-1.5 font-sans font-bold">© 2021 | All Rights Reserved.</p>                               
                     </div>
                 </div>
-            </div>  
+            </div>
+            
         </div>
     </footer>
 </x-app-layout>
